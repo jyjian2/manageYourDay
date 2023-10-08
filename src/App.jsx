7126940e4ec841aba33f72647a22bbaab8cfeab7
Jia-Yi Jian <jyjian@illinois.edu>
2023-10-07 23:49:33 -0700

    added post request for adding new note

--- src/App.jsx
+++ src/App.jsx
@@ -12,45 +12,50 @@ const App = () => {
 
   useEffect(() => {
     axios
-    .get("http://localhost:3001/notes")
-    .then( response => {
-      setNotes(response.data)
-      //initial state will be ignore after react component render
-      //render time: change in their state or props
-    })
+      .get("http://localhost:3001/notes")
+      .then(response => {
+        setNotes(response.data)
+        //initial state will be ignore after react component render
+        //render time: change in their state or props
+      })
   }, [])
 
   const notesToShow = showAll
-  ? notes : notes.filter(note => note.important)
+    ? notes : notes.filter(note => note.important)
 
   const addNote = (event) => {
     event.preventDefault();
-    const newObject = {
+    const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1
     }
-    setNotes(notes.concat(newObject))
-    setNewNote('')
+
+    //send new note object to server
+    axios
+      .post("http://localhost:3001/notes", noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
-  
+
   return (
     <div>
       <h1>Notes</h1>
-      <button onClick={()=>setShowAll(!showAll)}>
+      <button onClick={() => setShowAll(!showAll)}>
         show {showAll ? 'important' : 'all'}
       </button>
       <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note}/>)}
+        {notesToShow.map(note => <Note key={note.id} note={note} />)}
       </ul>
 
       <form onSubmit={addNote}>
-        <input value={newNote} 
-              onChange={handleNoteChange}/>
+        <input value={newNote}
+          onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
