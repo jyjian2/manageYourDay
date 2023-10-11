7fbef4c3eb5e38ba4e95e5416fa41b82a2cb1770
Jia-Yi Jian <jyjian@illinois.edu>
2023-10-11 16:11:42 -0700

    wrote notes object definition a more compact syntax

--- src/services/notes.js
+++ src/services/notes.js
@@ -17,8 +17,4 @@ const update = (id, newObject) => {
     return request.then(response => {return response.data})
 }
 
-export default {
-    getAll: getAll,
-    create: create,
-    update: update
-}
+export default {getAll, create, update}
